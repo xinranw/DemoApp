228882f39ef88cd7df0eff2dc8d6a76dcbd03ae8
Xinran Wang <xw1992@gmail.com>
2013-11-04 22:23:48 -0500

    Initial Commit

---
+++ DemoApp/XIWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XIWAppDelegate.h
+//  DemoApp
+//
+//  Created by User on 11/4/13.
+//  Copyright (c) 2013 Xinran Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XIWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoApp
+//
+//  Created by User on 11/4/13.
+//  Copyright (c) 2013 Xinran Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XIWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XIWAppDelegate class]));
+    }
+}
